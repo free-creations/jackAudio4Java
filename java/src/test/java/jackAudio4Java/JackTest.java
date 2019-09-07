abbd4f9c1f4891d87e4d16774856a4801619bd20
Harald <Harald-lb@free-creations.de>
2019-09-07 15:57:13 +0200

    now using google Truth for assertions.

--- java/src/test/java/jackAudio4Java/JackTest.java
+++ java/src/test/java/jackAudio4Java/JackTest.java
@@ -3,6 +3,8 @@ package jackAudio4Java;
 import jackAudio4Java.types.Int;
 import org.junit.Before;
 import org.junit.Test;
+import static com.google.common.truth.Truth.assertThat;
+
 
 import java.util.logging.Level;
 
@@ -27,10 +29,10 @@ public class JackTest {
 
     Jack.server().getJackVersion(major_ptr, minor_ptr, micro_ptr, proto_ptr);
 
-    assertTrue(major_ptr.value >= 0);
-    assertTrue(minor_ptr.value >= 0);
-    assertTrue(micro_ptr.value >= 0);
-    assertTrue(proto_ptr.value >= 0);
+    assertThat(major_ptr.value).isAtLeast(0);
+    assertThat(minor_ptr.value).isAtLeast(0);
+    assertThat(micro_ptr.value).isAtLeast(0);
+    assertThat(proto_ptr.value).isAtLeast(0);
   }
 
   /**
@@ -43,14 +45,14 @@ public class JackTest {
     Int minor_ptr = new Int(-1);
 
     Jack.server().getJackVersion(major_ptr, minor_ptr, null, null);
-
-    assertTrue(major_ptr.value >= 0);
-    assertTrue(minor_ptr.value >= 0);
+    assertThat(major_ptr.value).isAtLeast(0);
+    assertThat(minor_ptr.value).isAtLeast(0);
+;
   }
 
   @Test
   public void jni_get_version() {
     int version = Jack.server().getJniVersion();
-    assertTrue(version >= 0);
+    assertThat(version).isAtLeast(0);
   }
 }
